b01cc5ab88704983557877f6e9543d6cce1be308
teejayvanslyke <teejay.vanslyke@gmail.com>
2009-10-05 22:55:10 -0700

    Good enough.

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,6 @@
 class Status < ActiveRecord::Base
   validates_uniqueness_of :twitter_id
+  belongs_to :trend
 
   def self.random
     find(:first, :order => 'rand()')
--- lib/poller.rb
+++ lib/poller.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment") unless defined?(RAILS_ROOT)
 
 StatusClassifier.start_service
-#DRb.thread.join
 
 loop do
   Trend.analyze_current
